14a0dd06a7d44384bf2552525d0220c2293ada19
hafskjfha <153927840+hafskjfha@users.noreply.github.com>
2024-09-09 15:51:25 +0900

    Create db.js

---
+++ test_login/db.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'your-db-user',
+  host: 'localhost',
+  database: 'your-db-name',
+  password: 'your-db-password',
+  port: 5432,
+});
+
+module.exports = pool;
