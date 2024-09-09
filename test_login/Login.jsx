546e9d00b2d1730daebb3206110c7e772e197b43
hafskjfha <153927840+hafskjfha@users.noreply.github.com>
2024-09-09 15:53:17 +0900

    Update Login.jsx

--- test_login/Login.jsx
+++ test_login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Login() {
     const [email, setEmail] = useState('');
@@ -31,6 +32,8 @@ function Login() {
                 onChange={(e) => setPassword(e.target.value)} 
             />
             <button onClick={handleLogin}>Login</button>
+            
+            <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
         </div>
     );
 }
